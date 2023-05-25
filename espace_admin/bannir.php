7fed92d53dded6110c420bafc4d6cf531b7c06f8
Anthony <semprini.anthony@gmail.com>
2023-05-25 23:51:30 +0200

    bannir membre en le suprimmant de la bdd

---
+++ espace_admin/bannir.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=espace_admin;charset=utf8;','root', 'root');//relie a la bdd
+if(isset($_GET['id']) AND !empty($_GET['id'])){
+    $getId = $_GET['id'];
+    $recuUser = $bdd->prepare('SELECT * FROM membres WHERE id = ?');
+    $recuUser->execute(array($getId));
+    if($recuUser->rowCount() > 0){
+
+        $bannirUser = $bdd->prepare('DELETE FROM membres WHERE id = ?');//delete le membres de la bdd SQL
+        $bannirUser->execute(array($getId));
+        header('Location: membres.php')
+    }else{
+        echo "Aucun membres n'a été trouvé";
+    }
+}else{
+    echo "L'identifiant n'a pas été récupéré ";
+}
