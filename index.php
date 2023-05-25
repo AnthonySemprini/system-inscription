f075b6835d82da8faa8c81e011e6ca8bd3cd1944
Anthony <semprini.anthony@gmail.com>
2023-05-25 14:10:04 +0200

    boutton deconex qui redirige vers connex.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!$_SESSION['mdp']){
+    header('Location: connexion.php');
+}
 echo "Hello ".$_SESSION['pseudo'];
 ?>
 
@@ -9,9 +12,10 @@ echo "Hello ".$_SESSION['pseudo'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceuil</title>
+    <title>Home</title>
 </head>
 <body>
-    
+    <h1>Home</h1>
+    <a href="deconnexion.php"><button>Se deconnecter</button></a>
 </body>
 </html>
