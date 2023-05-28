27aab5501fe87fc0eed60c40044fcf556b8d4e8b
Anthony <semprini.anthony@gmail.com>
2023-05-29 00:20:22 +0200

    supprimer article crea d'un boutton dans article.php et cree page sup.php

--- espace_admin/articles.php
+++ espace_admin/articles.php
@@ -13,12 +13,18 @@ ob_start();
 <body>
     <?php
     $bdd = new PDO('mysql:host=localhost;dbname=espace_admin;charset=utf8;','root', 'root');//relie a la bdd
-    $recupArticles = $bdd = $bdd->query('SELECT *FROM articles');
-    while($article = $recupArticles->fetch()){
+    $recupArticles = $bdd = $bdd->query('SELECT *FROM articles');//recup dans la bdd
+    while($article = $recupArticles->fetch()){// boucle while 
         ?>
         <div class="article">
-            <h1><?=$article['titre']; ?></h1>
-            <p><?= $article['contenu']; ?></p>
+            <h1><?=$article['titre']; ?></h1><!--affiche titre-->
+            <p><?= $article['contenu']; ?></p><!--affiche contenu-->
+            <a href="supprimer-articles.php?id=<?= $article['id']; ?>">
+            <button style="color :red;" >Supprimer article</button>
+            </a>
+            <a href="modifier-articles.php?id=<?= $article['id']; ?>">
+            <button style="color :green;" >Modifier article</button>
+            </a>
         </div>
     <?php
     }
--- espace_admin/supprimer-articles.php
+++ espace_admin/supprimer-articles.php
@@ -2,6 +2,24 @@
 ob_start();
 ?>
 <?php
+ $bdd = new PDO('mysql:host=localhost;dbname=espace_admin;charset=utf8;','root', 'root');//relie a la bdd
+if(isset($_GET['id']) AND !empty($_GET['id'])){
+    $getID = $_GET['id'];
+    $recupArticle = $bdd->prepare('SELECT * FROM articles WHERE id = ?');//recup id dans bdd
+    $recupArticle->execute(array($getID));
+    if($recupArticle->rowCount() > 0){
+        $deleteArticle =$bdd->prepare('DELETE FROM articles WHERE id = ?');// efface article dans bdd grace a l'ID
+        $deleteArticle->execute(array($getID));
+        header('Location: articles.php');
+    }else{
+        echo "Aucun article trouvé";
+    }
+}else{
+    echo "Aucun identifiant trouvé";
+}
+
+?>
+<?php
 $content = ob_get_clean();
 require "template.php";
 ?>
